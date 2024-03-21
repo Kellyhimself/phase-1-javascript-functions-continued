c42abfd577d74279faca47302bfce349ddee5b2b
Kitui Kelly <kituikelly@gmail.com>
2024-03-21 21:26:18 +0300

    completes lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+ 
+
+function saturdayFun(msg = 'roller-skate'){
+    
+    return `This Saturday, I want to ${msg}!`;
+ }
+
+
+ const mondayWork = function(msg2='go to the office'){
+    return `This Monday, I will ${msg2}.`;
+ }
+ 
+
+function wrapAdjective(visualFlair = "*") {
+    
+  return function (adjective = "special") {
+    return `You are ${visualFlair}${adjective}${visualFlair}!`;
+  };
+const encouragingPromptFunction = wrapAdjective("!!!");
+}
